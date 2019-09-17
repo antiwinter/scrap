69dc585fcac8c485a07be09dcc689bf535f85496
travis <travis@travis-ci.org>
2019-09-17 17:46:54 +0800

    [ci skip] update

--- wowa/push.sh
+++ wowa/push.sh
@@ -5,12 +5,6 @@ git config --global user.name "travis"
 git checkout master
 git add .
 git commit -m "[ci skip] update"
-
-# Attempt to commit to git only if "git commit" succeeded
-if [ $? -eq 0 ]; then
-  git remote rm origin
-  git remote add origin https://antiwinter:${GITHUB_TOKEN}@github.com/antiwinter/scrap.git
-  git push origin master
-else
-  echo "no change"
-fi
+git remote rm origin
+git remote add origin https://antiwinter:${GITHUB_TOKEN}@github.com/antiwinter/scrap.git
+git push origin master
