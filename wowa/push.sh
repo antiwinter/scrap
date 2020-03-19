d2fc5e1825ae2db6d2a894cef10def0ad925647a
travis <travis@travis-ci.org>
2020-03-19 10:07:46 +0800

    support gitee

--- wowa/push.sh
+++ wowa/push.sh
@@ -5,6 +5,14 @@ git config --global user.name "travis"
 git checkout master
 git add .
 git commit -m "[ci skip] update"
+
+#push to github
 git remote rm origin
 git remote add origin https://antiwinter:${GITHUB_TOKEN}@github.com/antiwinter/scrap.git
 git push origin master
+
+#push to gitee
+git remote rm origin
+git remote add origin https://antiwinter:${GITEE_TOKEN}@gitee.com/antiwinter/scrap.git
+git push origin master
+
